2940b66b21a6a3d197394fb747613b23395fbc5c
mmatusik <mmatusikpl@gmail.com>
2018-04-05 12:32:37 +0200

    Gallery masonry grid fix

--- page-templates/gallery.php
+++ page-templates/gallery.php
@@ -84,52 +84,35 @@ foreach ($query->posts as $key => $post) {
 	<div class="btn-toolbar filters">
 		<div data-toggle="buttons" class="btn-group">
 			<label class="btn on">
-				<input name="filter" value="*" checked="checked" type="radio">
+				<input name="filter" data-filter="*" checked="checked" type="radio">
 				Wszystkie
 			</label>
 			<?php foreach ($all_terms as $slug => $name) : ?>
 			<label class="btn on">
-				<input name="filter" value="<?php echo $slug; ?>" type="radio">
+				<input name="filter" data-filter=".<?php echo $slug; ?>" type="radio">
 				<?php echo $name; ?>
 			</label>
 			<?php endforeach; ?>
 		</div>
 	</div>
 	<div class="grid-wrap max-width">
-		<div id="gallery-grid" class="grid photoswipe-wrapper images" itemscope itemtype="http://schema.org/ImageGallery">
-			<?php foreach ($gallery as $key => $image) : //print_r($image); ?>
-			<div class="grid-item photoswipe-item <?php //echo $widths[($key % 7)]; ?> <?php //echo $heights[($key % 7)]; ?> <?php echo $image['term']->slug; ?>">
+		<div id="masonry_gallery" class="max-width photoswipe-wrapper images" itemscope itemtype="http://schema.org/ImageGallery"> 
+			<div class="grid-sizer"></div> 
+			<div class="gutter-sizer"></div> 
+
+			<?php foreach ($gallery as $key => $image) : ?>
+			<div class="grid-item photoswipe-item <?php echo $image['term']->slug; ?>">
 				<a href="<?php echo $image['sizes']['yumi-gallery-item']; ?>" data-size="<?php echo $image['sizes']['yumi-gallery-item-width']; ?>x<?php echo $image['sizes']['yumi-gallery-item-height']; ?>" class="" style="background-image: url('<?php echo $image['sizes']['yumi-gallery-item']; ?>');">
-					<div class="overlay"><i class="icon-search"></i></div>
+					<div class="grid-item-inner">
+						<div class="overlay"><i class="icon-search"></i></div>
+					</div>
 				</a>
-	
 			</div>
 			<?php endforeach; // End of the loop. ?>
 		</div>
 	</div>
 
 <?php get_template_part( 'template-parts/page/content', 'photoswipe' ); ?>
-<div class="button-group filter-button-group">
-							Filter: 
-						  	<button data-filter="*">show all</button>
-						  	<button data-filter=".category-a">Category A</button>
-						  	<button data-filter=".category-b">Category B</button>
-						  	<button data-filter=".category-c">Category C</button>
-						</div>
-<div id="masonry_hybrid_demo3"> 
- <div class="grid-sizer"></div> 
- <div class="gutter-sizer"></div> 
- <div class="grid-item"><div class="grid-item-inner category-a">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-a">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-b">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-b">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-b">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-b">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-a">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-a">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-a">...</div> </div> 
- <div class="grid-item"><div class="grid-item-inner category-b">...</div> </div> 
-</div>
 <script>
 (function($) {
 	$(document).ready(function() {
@@ -170,29 +153,25 @@ foreach ($query->posts as $key => $post) {
 				
 		// 	});
 		// });
-		var $grid_demo_resize = $('#masonry_hybrid_demo3');
+		var $grid_demo_resize = $('#masonry_gallery');
 		var grid3 = new MasonryHybrid($grid_demo_resize, {col: 4, space: 30});
 		// Use resize
 		var grid_resize = grid3.resize({
 			celHeight 	: 180,
 			sizeMap 	: [[1,2],[2,1],[1,2],[1,2],[1,1],[1,1],[2,1]],
-			resize 		: true,
+			resize 		: false,
 		});
 		// Get Size Map
 		grid_resize.getSizeMap();
 		// Set Size Map & apply Size Map
-		grid_resize.setSizeMap([[2,2]]).applySize();
-		$('button').on('click', function() {
+		grid_resize.setSizeMap([[1,2],[2,1],[1,2],[1,2],[1,1],[1,1],[2,1]]).applySize();
+		$('.filters input').on('click', function() {
 			var filterValue = $(this).attr('data-filter');
-			alert(filterValue);
 			grid3.grid.isotope({
 				filter: filterValue
 			});
 		});
-		// Filter
-		grid3.grid.isotope({ filter: "filterValue" });
-
-
+		grid3.grid.isotope();
 	});
 })(jQuery);
 </script>
